5075b5ab637e1fb55651c54db3da52e710d7c5e2
zwpCode <384954802@qq.com>
2019-03-15 13:15:13 +0800

    shiro login logout

---
+++ src/test/java/test.java
@@ -0,0 +1,26 @@
+import redis.clients.jedis.Jedis;
+
+public class test {
+    public static void main(String[] args) {
+
+        //Jedis jedis = new Jedis("202.5.17.10",6379);
+
+       /* System.out.println(jedis.get("class"));
+
+        jedis.set("class","二班");
+
+        System.out.println(jedis.get("class"));*/
+
+        /*jedis.lpush("site-list","google");
+        jedis.lpush("site-list","baidu");
+        jedis.lpush("site-list","qq");
+
+        List<String> list = jedis.lrange("site-list",0,2);
+        for (int i = 0; i < list.size(); i++ ){
+            System.out.println(list.get(i));
+        }
+
+        jedis.close();*/
+
+    }
+}
